466e1927bdb138ad9774327a8fae3e11c02eeb50
HikarusV <nazwapradana.pc@gmail.com>
2025-02-16 12:29:30 +0700

    feat: api works base path

--- src/app.js
+++ src/app.js
@@ -38,6 +38,13 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/event', eventRouter);
 app.use('/api/v1/program', programRouter);
 app.use('/api/v1/job', jobRouter);
+app.use('/', (req, res) => {
+    res.json({ 
+        error: false,
+        status: 200,
+        message: "API works" 
+    });
+  });
 
 app.use(notFound);
 app.use(errorHandler);
