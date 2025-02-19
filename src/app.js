4f553611dae95668f35b65e5421771c2ca91d36e
Lutfi Cahya Nugraha <halolutfii@gmail.com>
2025-02-19 14:57:04 +0700

    refactor: back to before

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(helmet())
 app.use(cors({
-    origin: ['https://dev.app.techbuddies.id:6700'],
+    origin: 'https://dev.app.techbuddies.id',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
